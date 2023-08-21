169498750f8e95421e9d949d9d95db4d7a2dab76
Suhani Kumari <suhanik252@gmail.com>
2023-08-21 18:43:30 +0530

    calcii by c++

---
+++ C++/suc++/calcii.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c,d,e,f,g,h,i,j,k,l,m,n;
+    cout<<"Enter the values of  a and b";
+    cin>>a>>b;
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    f=a%b;
+    g=a/b;
+    h=a|b;
+    i=a&b;
+    j=a^b;
+    k=a<<2;
+    l=a>>2;
+    m=~a;
+    n=~b;
+
+
+    cout<<c<<"\n";
+    cout<<d<<"\n";
+    cout<<e<<"\n";
+    cout<<f<<"\n";
+    cout<<g<<"\n";
+    cout<<h<<"\n";
+    cout<<i<<"\n";
+    cout<<j<<"\n";
+    cout<<k<<"\n";
+    cout<<l<<"\n";
+    cout<<m<<"\n";
+    cout<<n<<"\n";
+}
