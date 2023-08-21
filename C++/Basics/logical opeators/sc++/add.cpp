d20d9e5ba5d57c71f5e4f8bc50a7d46fccd1b589
Suhani Kumari <suhanik252@gmail.com>
2023-08-21 18:23:10 +0530

    calci

---
+++ C++/Basics/logical opeators/sc++/add.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c,d,e,f,g,h,i,j,k,l,m,n;
+    cout<<"Enter the values of  a and b";
+    cin>>a>>b;
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    f=a%b;
+    g=a/b;
+    h=a|b;
+    i=a&b;
+    j=a^b;
+    k=a<<2;
+    l=a>>2;
+    m=~a;
+    n=~b;
+
+
+    cout<<c<<d<<e<<f<<g<<h<<i<<j<<k<<l<<m<<n;
+}
